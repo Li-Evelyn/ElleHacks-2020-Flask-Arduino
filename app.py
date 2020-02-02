7c1b0e04997613644197a7639053d5d57f7016b5
Li-Evelyn <lievelynpl@gmail.com>
2020-02-01 23:59:24 -0500

    zzz

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import serial
+import time
 
 app = Flask(__name__)
 
+def change_lock_state(state):
+    ser = serial.Serial('COM4', 9600)
+    if state == "open":
+        time.sleep(0.1)
+        ser.write(b'H')
+    else:
+        time.sleep(0.1)
+        ser.write(b'L')
 
 @app.route('/')
 def hello_world():
+    change_lock_state("open")
+    time.sleep(2)
+    change_lock_state("")
     return 'Hello World!'
 
 
